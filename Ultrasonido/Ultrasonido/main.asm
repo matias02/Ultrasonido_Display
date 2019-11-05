f8bff690b1e45c4556153af76bc5b41aea75fe70
Matias Cam <matias.cam@utec.edu.pe>
2019-11-05 12:20:17 -0500

    Add echo try

--- Ultrasonido/Ultrasonido/main.asm
+++ Ultrasonido/Ultrasonido/main.asm
@@ -8,24 +8,27 @@
 ; Replace with your application code
 .ORG   0x0000
    	RJMP   START
-//.ORG   INT0addr
-  // 	RJMP   INT_ECHO
+.ORG   INT0addr
+   	RJMP   INT_ECHO
 .ORG   OC2Aaddr
   	RJMP   T2_COMPA
 .ORG   OC1Aaddr
   	RJMP   T1_COMPA
 
+;.......................................	
+;Definicion de registros
+;.......................................
+.DEF   TMP = r20
+.DEF   ESTADO = r21
+.DEF   FLAG = r22
+.DEF   OVT = r23
+.DEF   DISTANCIA = r23
 
-.DEF   temp = r23
-.DEF   estado = r20
-.DEF   FLAG = r28
-.DEF   ovt = r21
 
 START:
-
-
-	
-	;Configuración del Timer1
+;.......................................	
+;Configuración del Timer1
+;.......................................
    	LDI      	r16, 0x00 ; Normal Mode, WGM11:WGM10 = 00
    	STS      	TCCR1A, r16
    	LDI      	r16, 0b00001101  ; Prescaler 8 - WGM13:WGM12 = 00
@@ -37,7 +40,9 @@ START:
 	LDI      	r16, 0xD1
    	STS      	OCR1AL, r16 ; Apaga pin PB0 en 10us
 
-; Configuración TIMER 2
+;.......................................	
+;Configuración del Timer1
+;.......................................
    	LDI      	r16, 0b00000010 ; CTC Mode
    	STS      	TCCR2A, r16
 	LDI      	r16, 0x01	;Prescaler 1
@@ -47,31 +52,46 @@ START:
    	LDI      	r16, 0x0A
    	STS      	OCR2A, r16   ; Apaga pin PB0 en 10us
 
-;COnf puertos
-	LDI estado, 0x01
+;.......................................	
+;Configuración de Puertos
+;.......................................
+	//RCALL		UART_CONFIG
+	LDI			ESTADO, 0x01
 	;Puerto D
 	LDI      	r16, 0x00 ; Define puertos B y D
    	OUT      	DDRD, r16
 	;Puerto B
    	LDI      	r17, 0x02
    	OUT      	DDRB, r17
-	clr			temp
-	out			portb, temp
-   	LDI      	temp, 0x00
-   	OUT      	DDRC, temp
+	clr			TMP
+	out			PORTD, TMP
+;.......................................	
+;Habilito interrupciones globales
+;.......................................
 	SEI
-	main:
-	rjmp		main
+;.......................................	
+;MAIN
+;.......................................
+	MAIN:
+	//RCALL		UART_TRANSMIT
+	RJMP		MAIN
+
+	
 
 T1_COMPA:
 	; eleva el trigger
 	SBI PORTB, 1
+
 	;Timer 2 en 10us
    	LDI      	r16, 0x01	;Prescaler 1
    	STS      	TCCR2B, r16
 	LDI      	r16, 0x0F
    	STS      	OCR2A, r16   ; Apaga pin PB0 en 10us
 
+	LDS			ESTADO, 0x01 ; Seteo estado en 1
+	
+
+	RETI
    	
 
 T2_COMPA:
@@ -85,7 +105,7 @@ Baja:
 	LDI      	r16, 0x00	;Prescaler 0
    	STS      	TCCR2B, r16
 	reti
-/*
+
 INT_ECHO:
 	; Guarda Registros
    	PUSH		r16
@@ -93,41 +113,62 @@ INT_ECHO:
    	PUSH		r16
  
    	CPI      	FLAG, 0x00
-   	BRNE     	flanco_bajada ;Salta a flanco de bajada cuando el echo regresa
-   	SER      	FLAG
-   	; Guardar cuenta
-   	LDS      	r24, TCNT1L
-   	LDS      	r25, TCNT1H
-   	; Reiniciar cuenta
-   	CLR      	r16
-   	STS      	TCNT1H, r16
-   	STS      	TCNT1L, r16
-   	STS      	TIMSK1, r16 ; Desactiva interrupcion por OCA
-   	RJMP     	salir1
+   	BRNE     	FLANCO_BAJADA ;Salta a flanco de bajada cuando el echo regresa
+	;Limpio OVT
+   	LDS			OVT, 0x00
+
+	;Modifico el timer 2 para cada centimetro
+	LDI      	r16, 0x01	;Prescaler 1
+   	STS      	TCCR2B, r16
+	LDI      	r16, 0x3A
+   	STS      	OCR2A, r16   ; Apaga pin PB0 en 10us
+
+	LDS			ESTADO, 0x00; Seteo estado en 0
+
  
-flanco_bajada:
+FLANCO_BAJADA:
    	CLR      	FLAG
-   	; Guardar nueva cuenta
-   	LDS      	ECHOL, TCNT1L	;guarda la cuenta en registro ECHOL y ECHOH
-   	LDS      	ECHOH, TCNT1H
-   	LDI      	r16, 0x02	; Desactiva interrupcion por OCA
+   	; Guardar Distancia
+	MOV			DISTANCIA, OVT
+	; Desactiva interrupcion por OCA
+   	LDI      	r16, 0x02	
    	STS      	TIMSK1, r16
-	CPI			ECHOL, 0x0F ; Salta a alarma cuando distancia menor a 15
-	BRLO     	ALARMA
- 
-salir1:
+
+
+ SALIDA:
    	; Recupera Registros
    	POP	   		r16
    	OUT      	SREG, r16
    	POP      	r16
    	RETI
  
-ALARMA:
-	MOV			temp, ECHOL
-	swap		temp
+ /*
 
-   	andi		temp, 0x01
-	//OUT      	PORTD, temp 	; Indica alarma
-   	RJMP     	salir1
+ 	
+UART_CONFIG:
+; Configuracion del UART con un BaudRate de 9600 a 1MHz, se activa la interrupcion para el recepcion del uart. 
+; Baud rate = 9600 @ 1MHz
+	ldi r16, 12; 
+	ldi r17, 0;
+	sts UBRR0H, r17
+	sts UBRR0L, r16
+; Enable transmitter and receiver with interrupt
+	ldi r16, (1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)
+	sts UCSR0B, r16
+; 8 data bits, 1 stop bit
+	ldi r16, (1<<UCSZ00)|(1<<UCSZ01)
+	sts UCSR0C, r16
+; se habilita la opcion de x2 la velocidad para mejorar el error del baud rate
+	ldi r18, (1<<U2X0)
+	sts UCSR0A, r18
+	ret
 
+UART_TRANSMIT:
+	; Esta subrutina se encarga de enviar un dato guardado en el registro r17 por Tx de UART
+	; data in r17
+	lds r16, UCSR0A
+	sbrs r16, UDRE0
+	rjmp UART_TRANSMIT
+	sts UDR0, DISTANCIA
+	ret
 	*/
